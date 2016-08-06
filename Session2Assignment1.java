e90b656e4146c641bad024a6406b6d398516ca6e
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-08-06 18:33:10 +0530

    Hi

---
+++ Session2Assignment1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Session2Assignment1
+{
+    public static void main(String [] args) 
+    {
+       Scanner sc = new Scanner(System.in);
+       int age;
+       System.out.print("Enter your age: ");
+       age = sc.nextInt();
+          
+        if (age >= 18)
+           {
+               System.out.println("You are eligible to vote.");
+           }
+        else
+           {
+               System.out.println("You are not eligible to vote.");     
+           }
+     }
+
+}
